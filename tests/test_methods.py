4921a5dccca34e04af169db210d9664cc1646bd6
Halley Fritze <97766437+hfr1tz3@users.noreply.github.com>
2024-12-22 11:43:19 -0800

    quick fix

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -125,7 +125,7 @@ def naive_compare(ts, other, transform=None, ties="average"):
     best_match_spans = np.zeros((ts.num_nodes,))
     time_discrepancies = np.zeros((ts.num_nodes,))
     for i, j in enumerate(best_match):
-        if ties is 'average':
+        if ties == 'average':
             best_match_spans[i] = share_spans[i, j]/np.bincount(best_match)[j]
         if ties is None:
             best_match_spans[i] = shared_spans[i, j]
